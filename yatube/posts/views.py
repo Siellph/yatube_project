e7bcc46eb18a8835096172122da79d6644a61fd6
Vladislav Grodin <gordin@it-impuls.ru>
2022-08-24 17:35:08 +0500

    Добавлены: админ-зона, заменена БД на тестовую, созданы модели управления и отображения информации из БД. Коммит перед заданием и финальным проектом 3го спринта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,20 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from multiprocessing import context
+from re import template
+from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context: dict = {
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Страница с постами группы {slug}')
+def group_posts(request):
+    context: dict = {
+        'description': 'Здесь будет информация о группах проекта Yatube'
+    }
+    template: str = 'posts/group_list.html'
+    return render(request, template, context)
